52f1e86c1961185029ebdba5e51aceba2e2e2996
Noe FEMELAND <percyfemeland@gmail.com>
2022-04-08 18:44:39 +0200

    Partie Option, fonctionnelle pour les proprios

--- src/Controller/OptionPriceController.php
+++ src/Controller/OptionPriceController.php
@@ -2,35 +2,38 @@
 
 namespace App\Controller;
 
+use App\Entity\Gite;
 use App\Entity\OptionPrice;
 use App\Form\OptionPriceType;
 use App\Repository\GiteRepository;
 use App\Repository\OptionPriceRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/option-price')]
 class OptionPriceController extends AbstractController
 {
-    #[Route('/', name: 'app_option_price_index', methods: ['GET'])]
-    public function index(OptionPriceRepository $optionPriceRepository, GiteRepository $giteRepository): Response
+    #[Route('/index/{id}', name: 'app_option_price_index', methods: ['GET'])]
+    public function index(Gite $giteEntity, OptionPriceRepository $optionPriceRepository, GiteRepository $giteRepository): Response
     {
         $gites = $giteRepository->findByOwner($this->getUser());
         $gites_array = [];
         foreach ($gites as $gite) {
-            if ($gite->getId() == $_GET["id_gite"]) {
+            // if ($gite->getId() == $_GET["id_gite"]) {
                 $gites_array[] = $gite;
-            }
+            // }
         }
+        
         return $this->render('option_price/index.html.twig', [
             'option_prices' => $optionPriceRepository->findByGite($gites_array),
+            'gite' => $giteRepository->findOneBy(['id' => $giteEntity->getId()])
         ]);
     }
 
-    #[Route('/new', name: 'app_option_price_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, OptionPriceRepository $optionPriceRepository): Response
+    #[Route('/new/{id}', name: 'app_option_price_new', methods: ['GET', 'POST'])]
+    public function new(Gite $gite, Request $request, OptionPriceRepository $optionPriceRepository): Response
     {
         $optionPrice = new OptionPrice();
         $form = $this->createForm(OptionPriceType::class, $optionPrice);
@@ -38,10 +41,10 @@ class OptionPriceController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             // PROBLEME AVEC INSERTION
-            $optionPrice->setGite($_GET["id_gite"]);
+            $optionPrice->setGite($gite);
 
             $optionPriceRepository->add($optionPrice);
-            return $this->redirectToRoute('app_option_price_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_option_price_index', ['id' => $gite->getId()]);
         }
 
         return $this->renderForm('option_price/new.html.twig', [
@@ -82,6 +85,6 @@ class OptionPriceController extends AbstractController
             $optionPriceRepository->remove($optionPrice);
         }
 
-        return $this->redirectToRoute('app_option_price_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_gite_index', [], Response::HTTP_SEE_OTHER);
     }
 }
